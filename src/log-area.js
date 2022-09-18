df44992bfdb1bf99d65f022bfeb665f090c6820e
devonrandall-design <111830060+devonrandall-design@users.noreply.github.com>
2022-09-18 18:27:08 -0400

    2 files modified

--- src/log-area.js
+++ src/log-area.js
@@ -5,17 +5,17 @@ export default function Log({currentLog}) {
 
   return ( <> <div className="log-area">
     <div className="table-row">
-    <div className="table-column"><p className="header-item">Name</p></div>
-       <div className="table-column"><p className="header-item">Set Number</p></div>
-       <div className="table-column"><p className="header-item">Weight</p></div>
-       <div className="table-column"><p className="header-item">Reps</p></div>
+    <div className="table-column name"><p className="header-item">Name</p></div>
+       <div className="table-column otherinfo"><p className="header-item">Set Number</p></div>
+       <div className="table-column otheriinfo"><p className="header-item">Weight</p></div>
+       <div className="table-column otherinfo"><p className="header-item">Reps</p></div>
       </div>
    {currentLog.map(log => {
      return <><div className="table-row">
-       <div className="table-column"><p className="list-item">{log._name}</p></div>
-       <div className="table-column"><p className="list-item">{log._set}</p></div>
-       <div className="table-column"><p className="list-item">{log._weight}</p></div>
-       <div className="table-column"><p className="list-item">{log._reps}</p></div>
+       <div className="table-column name"><p className="list-item">{log._name}</p></div>
+       <div className="table-column otherinfo"><p className="list-item">{log._set}</p></div>
+       <div className="table-column otherinfo"><p className="list-item">{log._weight}</p></div>
+       <div className="table-column otherinfo"><p className="list-item">{log._reps}</p></div>
      </div>
      </>})}
   </div>
