49c9214f66b8758479b01d2dfad757f6cbab84b0
devonrandall-design <111830060+devonrandall-design@users.noreply.github.com>
2022-09-18 18:07:03 -0400

    2 files modified

--- src/modal.js
+++ src/modal.js
@@ -5,6 +5,7 @@ export default function Modal({modal, changeModal, children}) {
   return (
   <div className={modal}>
     <div className="modal-header">
+      <h2>Workout Log</h2>
       <button onClick={() => changeModal('modal') }>X</button>
     </div>
     {children}
