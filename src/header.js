07c31d367c440ba26a7e606e08d43381e9e8ac5e
devonrandall-design <111830060+devonrandall-design@users.noreply.github.com>
2022-09-18 18:39:53 -0400

    2 files modified

--- src/header.js
+++ src/header.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function Header({noModal, modal}) {
+export default function Header({noModal, modal, exerciseName}) {
 
 return <div className="header">
   <div className="header-item-sm"></div>
-  <div className="header-item-big"><h1>Exercise Title</h1></div>
+  <div className="header-item-big"><input Exercise Title /></div>
   <div className="header-item-sm"><button className="view" onClick={() => noModal(modal === 'modal' ? 'modal-show' : 'modal')}>View Log</button></div>
 </div>
 
