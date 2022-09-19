389c8ee71cbd4d93657547912a08bf13404aa3e2
devonrandall-design <111830060+devonrandall-design@users.noreply.github.com>
2022-09-18 20:53:48 -0400

    2 files modified

--- src/log-area.js
+++ src/log-area.js
@@ -3,13 +3,15 @@ import React from 'react'
 
 export default function Log({currentLog}) {
 
-  return ( <> <div className="log-area">
+  return ( <> <div className="table-header">
     <div className="table-row">
     <div className="table-column name"><p className="header-item">Name</p></div>
        <div className="table-column otherinfo"><p className="header-item">Set Number</p></div>
-       <div className="table-column otheriinfo"><p className="header-item">Weight</p></div>
+       <div className="table-column otherinfo"><p className="header-item">Weight</p></div>
        <div className="table-column otherinfo"><p className="header-item">Reps</p></div>
       </div>
+      </div>
+      <div className="log-area">
    {currentLog.map(log => {
      return <><div className="table-row">
        <div className="table-column name"><p className="list-item">{log._name}</p></div>
