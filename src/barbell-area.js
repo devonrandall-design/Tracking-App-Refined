a5ad1bb8253828f4ddc43309102a08c03be6f098
devonrandall-design <111830060+devonrandall-design@users.noreply.github.com>
2022-09-18 21:07:12 -0400

    2 files modified

--- src/barbell-area.js
+++ src/barbell-area.js
@@ -3,6 +3,9 @@ import React from 'react'
 export default function Barbell() {
 
   return <div className="bar-area">
-    <div className="bar"></div>
+    <div className="bar">
+      <div className="clamp left"></div>
+      <div className="clamp right"></div>
+    </div>
   </div>
 }
