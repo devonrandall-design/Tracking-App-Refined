42c33db602b2053fc0aa65b996c2cd7de828fe85
devonrandall-design <111830060+devonrandall-design@users.noreply.github.com>
2022-09-19 14:55:03 -0400

    Update barbell-area.js

--- src/barbell-area.js
+++ src/barbell-area.js
@@ -2,22 +2,12 @@ import React from 'react'
 
 export default function Barbell({leftbar, rightbar, distance, changeDistance}) {
 
-  let y = 60
-
-  /* const styles = { 
-    transform: `translate(${x}px, ${y}px)` 
-}; */
 
   
 
   return <div className="bar-area">
     <div className="bar">
-{
-leftbar.map(bar => { 
-  return <div className={`plate-${bar} ` + 'left-plate'}>
-
-</div>}
-)}
+{leftbar.map(bar => { return <div className={`plate-${bar} ` + 'left-plate'} > </div>})}
       <div className="clamp left"></div>
       <div className="clamp right"></div>
 {rightbar.map(bar => { return <div className={`plate-${bar} ` + 'right-plate'}></div>})}
