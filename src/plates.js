f12e6c46bcb88364d650df61ef61f8908a769037
devonrandall-design <111830060+devonrandall-design@users.noreply.github.com>
2022-09-20 21:30:42 -0400

    2 files modified

--- src/plates.js
+++ src/plates.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 export default function Plates({amount, selectWeight, selectedName, id, current}) {
 
+
+  
   return <div onClick={() => selectWeight(amount)}  
   className={`plate ${ amount === current / 2 ? 'selected-plate': ''}`}>
     <h3>{amount}</h3></div>
 }
+
+
